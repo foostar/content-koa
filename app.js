4731132c57974c98492934e48da022aea33fa121
yinz <uinz.cn@gmail.com>
2017-02-12 18:45:57 +0800

    fix

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(config.MONGO_DB);
+
 require('colors');
 require('app-module-path/register');
 
 const Koa = require('koa');
-const mongoose = require('mongoose');
 const bodyParser = require('koa-bodyparser');
 const logger = require('koa-logger');
-
-mongoose.Promise = global.Promise;
 
 const config = require('config');
 const error = require('middleware/error');
@@ -24,5 +25,3 @@ app.use(router.routes());
 app.listen(8080, () => {
     console.log('server start at 8080');
 });
-
-mongoose.connect(config.MONGO_DB);
