d5df47d9f0747c30c9f1af3a18e4e76450105430
yinz <uinz.cn@gmail.com>
2017-02-12 20:45:17 +0800

    read readme once

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const router = new Router();
 const path = require('path');
 const fs = require('fs');
 
+const homeText = fs.readFileSync(
+    path.resolve(__dirname, '../README.md'),
+    'utf-8'
+);
+
 router.get('/', ctx => {
-    ctx.body = fs.readFileSync(
-        path.resolve(__dirname, '../README.md'),
-        'utf-8'
-    );
+    ctx.body = homeText;
 });
 
 module.exports = router;
