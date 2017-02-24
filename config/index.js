bbcec8ebc2fec0ee1572f0125fa1ae97495487a1
殷众 <yinzhong@goyoo.com>
2017-02-24 16:14:19 +0800

    up

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
-module.exports = process.env.NODE_ENV === 'production'
+const config = process.env.NODE_ENV === 'production'
     ? require('./prod')
     : require('./dev');
 
+module.exports = config;
