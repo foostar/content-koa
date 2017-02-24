78b53c31c04b3e887b1b68b687fd1e39fba4211f
殷众 <yinzhong@goyoo.com>
2017-02-24 11:48:04 +0800

    add mongodb

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
-FROM node:7.4
+FROM node:7.5
+
 RUN mkdir -p /usr/src/app
+
 WORKDIR /usr/src/app
+
 COPY package.json /usr/src/app/
 
 RUN npm install --production --registry=https://registry.npm.taobao.org
--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    JWT_SECRET: '',
-    MONGO_DB: ''
+    JWT_SECRET: '9104b291d48a4844d2a346a695d2602fa4d8ffe7328f7c98c27270b32a6b636f', // sha256 gp-content-gp-content-distribution
+    MONGO_DB: 'mongodb://gpcontent@c9yJ2gBFkp7U6mongo-gp-content-distribution-1.localhost:1302,mongo-gp-content-distribution-2.localhost:1302,mongo-gp-content-distribution-3.localhost:1302/content-distribution'
 };
 
