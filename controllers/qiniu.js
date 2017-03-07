a789894ad2f92522a262249dbd8b7aaab71bd79b
xiusiteng <xiusiteng@goyoo.com>
2017-03-07 14:05:22 +0800

    qiniu replaceimg done

--- controllers/qiniu.js
+++ controllers/qiniu.js
@@ -1,4 +1,6 @@
 const qiniu = require('qiniu');
+const fetch = require('node-fetch');
+const utils = qiniu.util;
 const {QINIU_ACCESS_KEY, QINIU_SECRET_KEY} = require('config');
 
 qiniu.conf.ACCESS_KEY = QINIU_ACCESS_KEY;
@@ -22,5 +24,35 @@ exports.uptoken = async (ctx, next) => {
             token
         }
     };
+};
+exports.replaceSrc = (ctx, next) => {
+    let {bucket, key, path} = ctx.request.body;
+    bucket = bucket || 'gp-baijia-public';
+    key = key || Date.now();
+    path = path || 'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=3889134221,835523178&fm=170&s=4F301BC78AB2E5FF5EADB8100300A0C3&w=480&h=585&img.JPEG';
+    path = utils.urlsafeBase64Encode(path);
+    const EncodedEntryURI = `${bucket}:${key}`;
+    const fetchUrl = `http://iovip.qbox.me/fetch/${path}/to/${EncodedEntryURI}`;
+    const token = qiniu.util.generateAccessToken(fetchUrl);
+    return fetch(fetchUrl, {
+        method: 'POST',
+        headers: {
+            'Authorization': token,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    .then(response => response.json())
+    .then(json => {
+        if (!json.key) {
+            ctx.assert(json, 400, '操作失败', {code: 1030001});
+        }
+        ctx.body = {
+            status: {
+                code: 0,
+                message: 'success'
+            },
+            data: json
+        };
+    });
 };
 
