483e97c9d930f7dfecd96265327723cd7e22753a
殷众 <yinzhong@goyoo.com>
2017-03-16 14:47:32 +0800

    七牛获取第三方图片 必须传 path

--- controllers/qiniu.js
+++ controllers/qiniu.js
@@ -7,10 +7,10 @@ qiniu.conf.ACCESS_KEY = QINIU_ACCESS_KEY;
 qiniu.conf.SECRET_KEY = QINIU_SECRET_KEY;
 
 exports.uptoken = async (ctx, next) => {
-    const {bucket, key} = ctx.query;
+    const {bucket = 'gp-baijia-public', key = Date.now()} = ctx.query;
 
-    ctx.assert(bucket, 400, '缺少 bucket 参数', {code: 1030001});
-    ctx.assert(key, 400, '缺少 key 参数', {code: 1030001});
+    // ctx.assert(bucket, 400, '缺少 bucket 参数', {code: 1030001});
+    // ctx.assert(key, 400, '缺少 key 参数', {code: 1030001});
 
     const token = new qiniu.rs.PutPolicy(`${bucket}:${key}`).token();
     ctx.body = {
@@ -30,28 +30,32 @@ exports.replaceSrc = async (ctx, next) => {
     let {bucket, key, path} = ctx.request.body;
     bucket = bucket || 'gp-baijia-public';
     key = key || Date.now();
-    path = path || 'https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=3889134221,835523178&fm=170&s=4F301BC78AB2E5FF5EADB8100300A0C3&w=480&h=585&img.JPEG';
+    ctx.assert(path, 'no path', 400);
     path = utils.urlsafeBase64Encode(path);
     const EncodedEntryURI = `${bucket}:${key}`;
     const fetchUrl = `http://iovip.qbox.me/fetch/${path}/to/${EncodedEntryURI}`;
     const token = qiniu.util.generateAccessToken(fetchUrl);
 
-    const res = await fetch(fetchUrl, {
-        method: 'POST',
-        headers: {
-            'Authorization': token,
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    }).then(response => response.json());
+    try {
+        const res = await fetch(fetchUrl, {
+            method: 'POST',
+            headers: {
+                'Authorization': token,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then(response => response.json());
 
-    ctx.assert(res.key, 400, '操作失败', {code: 1030001});
+        ctx.assert(res.key, 400, '七牛服务错误', { code: 1030001 });
 
-    ctx.body = {
-        status: {
-            code: 0,
-            message: 'success'
-        },
-        data: res
-    };
+        ctx.body = {
+            status: {
+                code: 0,
+                message: 'success'
+            },
+            data: res
+        };
+    } catch (err) {
+        ctx.throw(err);
+    }
 };
 
--- test/qiniu.js
+++ test/qiniu.js
@@ -9,15 +9,11 @@ describe('七牛图片测试', function () {
                 .send({username: 'test', password: '123456'});
         token = res.body.data.token;
     });
-    it('默认返回豆瓣金刚狼图片', async function () {
+    it('return error when no path param', async function () {
         return request(app)
                 .post('/api/qiniu/replace-src')
                 .set('Authorization', `Bearer ${token}`)
-                .expect(200)
-                .expect(function (res) {
-                    if (res.body.status.code !== 0) throw new Error("code isn't 0");
-                    if (!res.body.data.key) throw new Error('上传失败');
-                });
+                .expect(400);
     });
     it('成功返回替换图片', async function () {
         const data = { path: 'http://mmbiz.qpic.cn/mmbiz/k0UVxv3BTLJs4606VnwPsT5ibjaDicX7GicAyicC0OpEFFWUEWxTZEb6OXxibdAdsuujgacoLv8vEmRytibqEH4al8WA/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1' };
